d26b3e1479094d0b14cb1b1ae2a5905d75036da9
nathanku3 <45957580+nathanku3@users.noreply.github.com>
2023-06-16 23:43:53 +0800

    Update node.c

--- node.c
+++ node.c
@@ -1,6 +1,7 @@
 #include "global.h"
 
-void saveNewNoteTitle(char *title, char *category) {
+
+void saveNewNoteTitle(char *title) {
     if (strlen(title) > 0 && strlen(title) <= MAX_TITLE_LENGTH - 1) {
         // Create a new node
         NoteNode *newNode = (NoteNode *) malloc(sizeof(NoteNode));
@@ -9,27 +10,26 @@ void saveNewNoteTitle(char *title, char *category) {
 		    return;
 		}
         strncpy(newNode->title, title, MAX_TITLE_LENGTH - 1);
-        strncpy(newNode->category, category, MAX_TITLE_LENGTH - 1);
         newNode->title[MAX_TITLE_LENGTH - 1] = '\0';
-        newNode->category[MAX_TITLE_LENGTH - 1] = '\0';
+        newNode->fp = NULL;
         newNode->next = NULL;
 
         // Add the new node to the linked list
         if (!note_head) {
             note_head = newNode;
         } else {
-
+        
 		NoteNode *current = note_head;
         while (current->next) {
             current = current->next;
         }
-
+        
 		current->next = newNode;
-
+        
         }
 
         noteCount++;
-
+        
         memset(titleInput, 0, sizeof(titleInput));
 
         Window_State = MAIN_WINDOW; // Return to the main window
@@ -60,3 +60,67 @@ void noteTitleClick(NoteNode *current){
 	;
 }
 
+void saveNote(NoteNode *current) {
+    char filename[MAX_TITLE_LENGTH + 5];  // +5 for extension and null terminator
+    snprintf(filename, sizeof(filename), "%s.txt", current->title);
+
+    if(current->fp == NULL) {
+        if((current->fp = fopen(filename, "w")) == NULL) {
+            drawErrorMessage(3);  // File open error
+            return;  // Exit the function instead of the whole program
+        }
+    } else {
+        if((current->fp = fopen(filename, "r+")) == NULL) {
+            drawErrorMessage(3);  // File open error
+            return;  // Exit the function instead of the whole program
+        }
+    }
+
+    if(current->fp != NULL) {
+        fwrite(text, 1, textlen, current->fp);
+        if(fclose(current->fp)) {
+            drawErrorMessage(4);  // Cannot close the file
+            return;  // Exit the function instead of the whole program
+        }
+        current->fp = NULL;
+    } else {
+        drawErrorMessage(3);  // File open error
+    }
+    Window_State = MAIN_WINDOW;
+}
+
+
+void loadNote(NoteNode *current) {
+    char filename[MAX_TITLE_LENGTH + 5];
+    snprintf(filename, sizeof(filename), "%s.txt", current->title);
+
+    if(current->fp == NULL) {
+        if((current->fp = fopen(filename, "r")) == NULL) {
+            drawErrorMessage(3);
+            return;
+        }
+    } else {
+        if((current->fp = fopen(filename, "r+")) == NULL) {
+            drawErrorMessage(3);
+            return;
+        }
+    }
+
+    if(current->fp != NULL) {
+        fseek(current->fp, 0, SEEK_END);
+        textlen = ftell(current->fp);
+        fseek(current->fp, 0, SEEK_SET);
+
+        fread(text, 1, textlen, current->fp);
+        text[textlen] = '\0';
+        location = textlen;
+
+        if(fclose(current->fp)) {
+            drawErrorMessage(4);
+            return;
+        }
+        current->fp = NULL;
+    } else {
+        drawErrorMessage(3);
+    }
+}
